d9821d3b65ee24ae3d76ff5f63bcabb616ea7a23
Hugo Carvalho <hugosc@gmail.com>
2020-05-17 16:47:01 -0300

    criada a função para converter milhas em metros

---
+++ calc.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#calc').click(function() {
+        const milhas = parseFloat($('#milhas').val())
+        const metros = 1609.34 * milhas
+        $('#metros').val(metros)
+    })
+})
